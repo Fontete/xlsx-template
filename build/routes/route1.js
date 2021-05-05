6dcbe73566e5847b78df563501b9b2acaa558c2e
Fontete <your_email_address@example.com>
2021-05-05 17:45:26 +0700

    Insert pivot to template

--- build/routes/route1.js
+++ build/routes/route1.js
@@ -9,50 +9,82 @@ var xlsx_template_1 = __importDefault(require("xlsx-template"));
 var fs_1 = __importDefault(require("fs"));
 var path_1 = __importDefault(require("path"));
 var xlsx_1 = __importDefault(require("xlsx"));
+// import xlsxChart from "xlsx-chart";
 // import {func1} from '../controllers/controller1'
 var router1 = express_1.Router();
 exports.router1 = router1;
 // router1.get("/", func1);
 router1.post("/", function (req, res) {
-    var workbook = xlsx_1.default.readFile("./raw.xlsx");
+    var workbook = xlsx_1.default.readFile("./template.xlsx");
     var sheet_name_list = workbook.SheetNames;
-    // type inputData = Object[];
+    // let sheet = {};
+    // for (let i = 0; i < sheet_name_list.length; i++) {
+    //   sheet = workbook.Sheets[sheet_name_list[i]];
+    //   xlsx.utils.sheet_to_json(sheet, {
+    //     header: "A",
+    //     raw: true,
+    //     blankrows: false,
+    //     // defval: null,
+    //   });
+    // }
     var sheet1 = workbook.Sheets[sheet_name_list[0]];
-    var rawData = xlsx_1.default.utils.sheet_to_json(sheet1, {
+    var sheet2 = workbook.Sheets[sheet_name_list[1]];
+    var sheet3 = workbook.Sheets[sheet_name_list[2]];
+    var sheet4 = workbook.Sheets[sheet_name_list[3]];
+    var sheet5 = workbook.Sheets[sheet_name_list[4]];
+    var sheet6 = workbook.Sheets[sheet_name_list[5]];
+    var sheet7 = workbook.Sheets[sheet_name_list[6]];
+    var sheet8 = workbook.Sheets[sheet_name_list[7]];
+    var sheet9 = workbook.Sheets[sheet_name_list[8]];
+    var sheet10 = workbook.Sheets[sheet_name_list[9]];
+    var rawData = xlsx_1.default.utils.sheet_to_json(sheet3, {
         header: "A",
-        //blankrows: false,
+        raw: true,
+        blankrows: false,
+        // defval: null,
     });
-    console.log("raw", rawData);
-    // const rawData = [
-    //   {
-    //     loivipham: "Bố trí phương tiện",
-    //     trogia: [2000, 3000],
-    //     ktg: [1000, 2000],
-    //   },
-    //   {
-    //     loivipham: "lạng lách",
-    //     trogia: [5000, 6000],
-    //     ktg: [1000, 2000],
-    //   },
-    // ];
+    var newData = [];
+    for (var i = 0; i < rawData.length; i++) {
+        if ((rawData[i].F && rawData[i].G) !== (null || undefined)) {
+            var obj = { F: rawData[i].F, G: rawData[i].G };
+            newData.push(obj);
+        }
+    }
+    console.log(newData);
+    var rawData2 = xlsx_1.default.utils.sheet_to_json(sheet4, {
+        header: "A",
+        raw: true,
+        blankrows: false,
+        defval: null,
+    });
+    var rawData3 = xlsx_1.default.utils.sheet_to_json(sheet5, {
+        header: "A",
+        raw: true,
+        blankrows: false,
+        defval: null,
+    });
+    //console.log("raw", rawData);
     fs_1.default.readFile(path_1.default.join(__dirname, "../../template.xlsx"), function (err, data) {
         var template = new xlsx_template_1.default(data);
-        var sheetNumber = 1;
+        var sheetNumber = 2;
         var values = {
-            // extractDate: new Date(),
-            // dates: [
-            //   new Date("2013-06-01"),
-            //   new Date("2013-06-02"),
-            //   new Date("2013-06-03"),
-            // ],
             rawData: rawData,
+            // newData,
+            // rawData2,
+            // rawData3,
+            // A: [
+            //   { A: "Đã hoàn thành" },
+            //   { A: "Đang tiến hành" },
+            //   { A: "Chưa thực hiện" },
+            // ],
+            // B: [{ B: 0.15 }, { B: 0.28 }, { B: 0.57 }],
         };
         console.log(values);
         template.substitute(sheetNumber, values);
         var result = template.generate({ type: "nodebuffer" });
         res.attachment("generateFile.xlsx");
         // xlsx.read(result, { type: "buffer" }).SheetNames.forEach((s) => {});
-        // console.log("result", xlsx.read(result, { type: "buffer" }).Sheets.Sheet1);
+        // console.log("result", xlsx.read(result, { typC: "buffer" }).Sheets);
         res.send(result);
     });
 });
--- src/routes/route1.ts
+++ src/routes/route1.ts
@@ -4,45 +4,82 @@ import XlsxTemplate from "xlsx-template";
 import fs from "fs";
 import path from "path";
 import xlsx from "xlsx";
+// import xlsxChart from "xlsx-chart";
+
 // import {func1} from '../controllers/controller1'
 const router1 = Router();
 
 // router1.get("/", func1);
 router1.post("/", (req: Request, res: Response) => {
-  const workbook = xlsx.readFile("./raw.xlsx");
+  const workbook = xlsx.readFile("./template.xlsx");
   const sheet_name_list = workbook.SheetNames;
-  // type inputData = Object[];
+  // let sheet = {};
+  // for (let i = 0; i < sheet_name_list.length; i++) {
+  //   sheet = workbook.Sheets[sheet_name_list[i]];
+  //   xlsx.utils.sheet_to_json(sheet, {
+  //     header: "A",
+  //     raw: true,
+  //     blankrows: false,
+  //     // defval: null,
+  //   });
+  // }
   const sheet1 = workbook.Sheets[sheet_name_list[0]];
-  const rawData = xlsx.utils.sheet_to_json(sheet1, {
+  const sheet2 = workbook.Sheets[sheet_name_list[1]];
+  const sheet3 = workbook.Sheets[sheet_name_list[2]];
+  const sheet4 = workbook.Sheets[sheet_name_list[3]];
+  const sheet5 = workbook.Sheets[sheet_name_list[4]];
+  const sheet6 = workbook.Sheets[sheet_name_list[5]];
+  const sheet7 = workbook.Sheets[sheet_name_list[6]];
+  const sheet8 = workbook.Sheets[sheet_name_list[7]];
+  const sheet9 = workbook.Sheets[sheet_name_list[8]];
+  const sheet10 = workbook.Sheets[sheet_name_list[9]];
+
+  const rawData = xlsx.utils.sheet_to_json(sheet3, {
     header: "A",
-    //blankrows: false,
+    raw: true,
+    blankrows: false,
+    // defval: null,
   });
-  console.log("raw", rawData);
-  // const rawData = [
-  //   {
-  //     loivipham: "Bố trí phương tiện",
-  //     trogia: [2000, 3000],
-  //     ktg: [1000, 2000],
-  //   },
-  //   {
-  //     loivipham: "lạng lách",
-  //     trogia: [5000, 6000],
-  //     ktg: [1000, 2000],
-  //   },
-  // ];
+
+  let newData: [] = [];
+  for (let i = 0; i < rawData.length; i++) {
+    if ((rawData[i].F && rawData[i].G) !== (null || undefined)) {
+      const obj = { F: rawData[i].F, G: rawData[i].G };
+      newData.push(obj);
+    }
+  }
+  console.log(newData);
+
+  const rawData2 = xlsx.utils.sheet_to_json(sheet4, {
+    header: "A",
+    raw: true,
+    blankrows: false,
+    defval: null,
+  });
+
+  const rawData3 = xlsx.utils.sheet_to_json(sheet5, {
+    header: "A",
+    raw: true,
+    blankrows: false,
+    defval: null,
+  });
+  //console.log("raw", rawData);
 
   fs.readFile(path.join(__dirname, "../../template.xlsx"), (err, data) => {
     const template: XlsxTemplate = new XlsxTemplate(data);
-    const sheetNumber: number = 1;
+    const sheetNumber: number = 2;
 
     const values = {
-      // extractDate: new Date(),
-      // dates: [
-      //   new Date("2013-06-01"),
-      //   new Date("2013-06-02"),
-      //   new Date("2013-06-03"),
-      // ],
       rawData,
+      // newData,
+      // rawData2,
+      // rawData3,
+      // A: [
+      //   { A: "Đã hoàn thành" },
+      //   { A: "Đang tiến hành" },
+      //   { A: "Chưa thực hiện" },
+      // ],
+      // B: [{ B: 0.15 }, { B: 0.28 }, { B: 0.57 }],
     };
     console.log(values);
 
@@ -51,7 +88,7 @@ router1.post("/", (req: Request, res: Response) => {
 
     res.attachment("generateFile.xlsx");
     // xlsx.read(result, { type: "buffer" }).SheetNames.forEach((s) => {});
-    // console.log("result", xlsx.read(result, { type: "buffer" }).Sheets.Sheet1);
+    // console.log("result", xlsx.read(result, { typC: "buffer" }).Sheets);
     res.send(result);
   });
 });
